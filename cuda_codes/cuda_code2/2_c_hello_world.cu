03cd796905caeeb60f647095d8e1722c6fba6b4b
Chekkala Sandeep Reddy <chekkalasandeepreddy@Chekkalas-MacBook-Air.local>
2024-09-15 10:36:14 +0530

    fixing typo

--- cuda_codes/cuda_code2/2_c_hello_world.cu
+++ cuda_codes/cuda_code2/2_c_hello_world.cu
@@ -8,6 +8,6 @@ __global__ void cuda_kernel(){
 
 int main(){
     cuda_kernel <<< 1, 5>>>(); // kernel launch (or) kernel invocation
-    cudaDeviceSynchronize();   // this is mush to make host wait until device completed its tasks
+    cudaDeviceSynchronize();   // this is must to make host wait until device completed its tasks
     return 0;
 }
